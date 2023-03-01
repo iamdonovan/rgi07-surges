af3c3dcb06a7236bcc41549ab4f4f8a4b2a59bea
Bob McNabb <robertmcnabb@gmail.com>
2023-03-01 22:01:42 +0000

    add script to join goerlich's outlines to region 13, based on percent overlap

---
+++ join_goerlich.py
@@ -0,0 +1,29 @@
+import os
+import geopandas as gpd
+import pandas as pd
+import numpy as np
+
+
+goerlich_outlines = gpd.read_file('Goerlich_2020/Goerlich_2020_GI-3/GI-3_rev.shp')
+
+# goerlich's outlines are only within RGI region 13
+rgi_dir = '/media/bob/seabox/RGI/'
+region = 'RGI2000-v7.0-G-13_asia_central'
+rgi_outlines = gpd.read_file(os.path.join(rgi_dir, 'v7b', region, region + '.shp'))
+
+# overlapping glaciers (to cut down on searching by distance later)
+overlap = rgi_outlines.sjoin(goerlich_outlines.to_crs(epsg=4326))
+overlap.to_crs(goerlich_outlines.crs, inplace=True)
+
+for ind, row in goerlich_outlines.iterrows():
+    # get all of the outlines that overlap with this one
+    overlapping = overlap.loc[overlap['glac_id'] == row['glac_id']]
+
+    # get the fraction of this outline's area that each overlap equals
+    overlaps = np.array([row['geometry'].intersection(o['geometry']).area for _, o in overlapping.iterrows()])
+    overlaps = overlaps / row['geometry'].area
+
+    # if an RGI outline has more than 5% overlap, include it
+    rgi_outlines.loc[overlapping.loc[overlaps > 0.05].index, 'surge_type'] = 3
+
+rgi_outlines.to_file(os.path.join('Goerlich_2020', region + '.gpkg'))

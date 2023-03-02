b023def353d018ea3c11a3dad5741930f0163b62
Bob McNabb <robertmcnabb@gmail.com>
2023-03-02 00:14:38 +0000

    join surge index to glacier table before exporting to csv, gpkg

--- refresh_data.py
+++ refresh_data.py
@@ -24,10 +24,25 @@ def load_csv(fn_csv, lon_col, lat_col):
 
 
 # first, copy the xlsx sheets to data/ in .csv format
-geodatabase_df = pd.read_excel('xlsx_docx/GeodatabaseSTglaciers.xlsx', sheet_name='Surge-type glaciers names')
-geodatabase_df.to_csv('data/GeodatabaseSTglaciers.csv', index=False)
+glaciers = pd.read_excel('xlsx_docx/GeodatabaseSTglaciers.xlsx', sheet_name='Surge-type glaciers names')
+surges = pd.read_excel('xlsx_docx/GeodatabaseSTglaciers.xlsx', sheet_name='Surges dates')
+
+# join the glaciers and surge tables, but drop any columns with no name
+joined = glaciers.set_index('Glacier_Name').join(surges.set_index('Glacier_Name'))
+keep_cols = [c for c in joined.columns if 'Unnamed:' not in c]
+joined = joined.loc[:, keep_cols]
+
+# get the maximum surge index for each glacier
+surge_index = joined.groupby('Glacier_Name')['Harmonised_Surge_Index'].max()
+
+# for some reason, some glaciers don't have a value here - set to 1 (otherwise why is it in the table?)
+surge_index[surge_index.isna()] = 1
+joined['SurgeType'] = surge_index
+joined.to_csv('data/GeodatabaseSTglaciers.csv', index=False)
+
 
 november_df = pd.read_excel('xlsx_docx/ST_November.xlsx')
+november_df['SurgeType'] = 1 # set to 1 by default, since we have no other information
 november_df.to_csv('data/ST_November.csv', index=False)
 
 # now, save the csv files as a gpkg
